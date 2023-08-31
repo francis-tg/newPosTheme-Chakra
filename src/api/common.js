64c83bf4dabe68cc02dab9c99eddfd20b0337b18
francis-tg <franc9885@gmail.com>
2023-08-31 12:06:26 +0000

    add prod url

--- src/api/common.js
+++ src/api/common.js
@@ -1,20 +1,20 @@
 export const BaseURL =
-  process.env.NODE_ENV === "production"
-    ? `http://${window.location.hostname}:5010`
+  process.env.NODE_ENV === 'production'
+    ? `https://placedelavictoire.onrender.com`
     : `http://${window.location.hostname}:5010`; //"https://place-de-la-victoire.onrender.com";
 export const API_URL = `${BaseURL}/api`;
 
 export function fetchAPI(method, url, data, options) {
-  if (method !== "GET") {
+  if (method !== 'GET') {
     return fetch(url, {
       method: method,
       headers: options,
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
   } else {
     return fetch(url, {
       method: method,
-      headers: options
+      headers: options,
     });
   }
 }
